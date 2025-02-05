683657b4dee8028a2e3e48665db86f4540fc8766
ipopov <igor.popov.develop@gmail.com>
2025-02-06 01:43:39 +0300

    commit

--- src/components/ActionBtn/ActionBtn.props.ts
+++ src/components/ActionBtn/ActionBtn.props.ts
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes, ReactNode } from 'react';
+import { InputHTMLAttributes } from 'react';
 
 export interface ActionBtnProps extends InputHTMLAttributes<HTMLButtonElement>{
     type: 'play' | 'done'
